850cf65fd369f6954b581f0830d0d184c41745e9
vivek-chaprana <vivek2003ji@outlook.com>
2024-03-20 09:58:22 +0530

    Fix: Youtube Video render error

--- src/components/BlogContentRenderer.tsx
+++ src/components/BlogContentRenderer.tsx
@@ -23,15 +23,21 @@
 // }
 
 // Option 2: Generating HTML from JSONContent
+import { WEBAPP_URL } from "@/lib/constants";
 import { JSONContent } from "@tiptap/core";
 import CharacterCount from "@tiptap/extension-character-count";
 import { Color } from "@tiptap/extension-color";
 import { FontFamily } from "@tiptap/extension-font-family";
+import Highlight from "@tiptap/extension-highlight";
+import Image from "@tiptap/extension-image";
 import { Link } from "@tiptap/extension-link";
+import Subscript from "@tiptap/extension-subscript";
+import Superscript from "@tiptap/extension-superscript";
 import { TextAlign } from "@tiptap/extension-text-align";
 import TextStyle from "@tiptap/extension-text-style";
 import Typography from "@tiptap/extension-typography";
 import Underline from "@tiptap/extension-underline";
+import Youtube from "@tiptap/extension-youtube";
 import { generateHTML } from "@tiptap/html";
 import StarterKit from "@tiptap/starter-kit";
 import DOMPurify from "isomorphic-dompurify";
@@ -59,9 +65,27 @@ export default function BlogContentRenderer({
       autolink: true,
     }),
     Typography,
+    Image.configure({
+      allowBase64: true,
+    }),
+    Superscript,
+    Subscript,
+    Highlight.configure({
+      multicolor: true,
+    }),
+    Youtube.configure({
+      controls: true,
+      allowFullscreen: false,
+      autoplay: false,
+      origin: WEBAPP_URL,
+      progressBarColor: "black",
+    }),
   ]);
 
-  const purifiedHtml = DOMPurify.sanitize(output);
+  const purifiedHtml = DOMPurify.sanitize(output, {
+    ADD_TAGS: ["iframe"],
+    ADD_ATTR: ["allow", "allowfullscreen", "frameborder", "scrolling"],
+  });
 
   return (
     <main
